3781fd93d6294e3d6b68d24d832038dcb6254b98
hyeonbell <qnduwnl@naver.com>
2019-07-16 22:46:58 +0900

    Printf problem
    Signed-off-by: hyeonbell <qnduwnl@naver.com>

---
+++ 12/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {	
+	fmt.Printf("%s%10s%15s\n", "이름","전공학과","학년")
+	fmt.Printf("%s%9s%15s\n","유현수","전자공학","3")
+	fmt.Printf("%s%10s%14s\n","김윤욱","컴퓨터공학","4")
+	fmt.Printf("%s%10s%14s\n","김나영","미술교육학","2")
+}
